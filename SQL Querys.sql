22879164683525aa677d2a9ef4d74c8362a7ad3a
vanesa-c <105691101+vanesa-c@users.noreply.github.com>
2025-02-15 13:57:38 +1100

    Task 1: SQL Programming.sql

---
+++ SQL Querys.sql
@@ -0,0 +1,73 @@
+-- 1. The names and the number of messages sent by each user---
+SELECT 
+  u.name, 
+  COUNT(DISTINCT m.MessageID) AS messages_sent
+FROM Users u
+LEFT JOIN Messages m 
+  ON u.UserID = m.UserIDSender
+GROUP BY 1
+ORDER BY 2 DESC
+
+-- 2. The total number of messages sent stratified by weekday---
+
+SELECT 
+    DAYNAME(timestamp) AS weekday, 
+    COUNT(DISTINCT MessageID) AS total_messages
+FROM Messages 
+GROUP BY weekday
+ORDER BY CASE 
+    WHEN weekday = 'Monday' THEN 1
+    WHEN weekday = 'Tuesday' THEN 2
+    WHEN weekday = 'Wednesday' THEN 3
+    WHEN weekday = 'Thursday' THEN 4
+    WHEN weekday = 'Friday' THEN 5
+    WHEN weekday = 'Saturday' THEN 6
+    WHEN weekday = 'Sunday' THEN 7
+END
+
+-- 3. The most recent message from each thread that has no response yet---
+
+
+WITH RecentMessages AS (
+    SELECT 
+        ThreadID,
+        MessageID,
+        UserIDSender,
+        UserIDRecipient,
+        MessageContent,
+        Timestamp
+    FROM Messages 
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY ThreadID ORDER BY Timestamp DESC) = 1
+)
+  
+SELECT 
+    r.*
+FROM RecentMessages r
+LEFT JOIN Messages m
+    ON r.ThreadID = m.ThreadID               -- join message of the same thread
+    AND r.UserIDSender = m.UserIDRecipient   -- checks that the  sender's message was replied to
+    AND r.Timestamp < m.Timestamp            -- only considers messages sent after r's message
+WHERE m.MessageID IS NULL
+
+
+
+-- 4. For the conversation with the most messages: all user data and message contents ordered chronologically---
+
+WITH ThreadMessageCounts AS (
+SELECT
+  ThreadID,
+  COUNT(DISTINCT MessageID) AS total_messages
+FROM Messages
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 1
+)
+
+SELECT 
+u.*, 
+m.*
+FROM Messages m
+LEFT JOIN Users u 
+  ON u.UserID = m.UserIDSender
+WHERE m.ThreadID = (SELECT ThreadID FROM ThreadMessageCounts)
+ORDER BY m.timestamp
